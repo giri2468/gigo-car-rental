461619bd7399a4f187d98343fd646a3f00e337c2
giri2468 <girigokul2468@gmail.com>
2022-07-31 11:43:43 +0530

    modified:   package-lock.json 	modified:   package.json     modified:   public/index.html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import Layout from "./components/Layout/Layout";
+import React from "react";
+import './App.css';
+import {BrowserRouter as Router , Route , Routes} from 'react-router-dom';
+import Home from './pages';
+import Signin from "./pages/signin";
+import Booking from "./pages/booking";
+import AboutCom from "./pages/aboutCom";
+import CarBook from "./pages/carBook";
 
 function App() {
-  return <Layout />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}  />
+        <Route path="/logout" element={<Signin/>}  />
+        <Route path="/booking" element={<Booking/>} />
+        <Route path='/aboutcompany' element={<AboutCom />} />
+        <Route path='/booknow/:slug' element={<CarBook />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
